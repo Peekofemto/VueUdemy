376056e5eb27eb1d5d027dca5476fbe634c5f31f
Peeko <omarock27@gmail.com>
2020-04-15 23:09:48 -0700

    Section 13 Improving your app with Filters and Mixins

---
+++ Exercises/Section_13_Improving_your_App_with_Filters_and_Mixins/src/List.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Filters & Mixins</h1>
+        <hr>
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {fruitMixin} from './fruitMixin';
+    export default {
+
+        mixins: [fruitMixin],
+        created(){
+            console.log('Inside List Created Hook');
+        }
+
+        // data(){
+        //     return{
+        //         fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+        //         filterText: ''
+        //     }
+        // },
+        // computed: {
+        //     filteredFruits(){
+        //         return this.fruits.filter((element) => {
+        //             return element.match(this.filterText);
+        //         });
+        //     }
+        // }
+    }
+</script>
+
+<style>
+
+</style>
